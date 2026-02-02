6cef9f8d6a8a3aaf1ca51409aefc28d128e40fc9
Darwin Mireles <d2mpyk@gmail.com>
2026-02-02 17:04:07 -0500

    NO FUNCIONAL: Edición de schemas/users.py para agregar role y is_active en el UserResponsePrivate del nuevo usuario para la confirmacion por correo.

--- schemas/user.py
+++ schemas/user.py
@@ -20,21 +20,18 @@ class UserResponsePublic(BaseModel):
     image_file: str | None
     image_path: str
 
-class UserResponsePrivate(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-
-    id: int
-    username: str
-    image_file: str | None
-    image_path: str
+class UserResponsePrivate(UserResponsePublic):
     email: EmailStr
     role: str
+    is_active: bool
     create_at: datetime
 
 # Actualización de usuario
 class UserUpdate(BaseModel):
     username: str | None = Field(default=None, min_length=1, max_length=50)
     email: EmailStr | None = Field(default=None, max_length=120)
+    role: str | None = Field(default=None)
+    is_active: bool | None = Field(default=None)
     image_file: str | None = Field(default=None, min_length=1, max_length=50)
 
 # Respuesta de inicio de sessión de usuario
