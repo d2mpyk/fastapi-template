105d6a0fc687802c43ce79ab57e846d5655cda95
Darwin Mireles <d2mpyk@gmail.com>
2026-02-02 16:57:49 -0500

    NO FUNCIONAL: Edición de config.py para gregar nuevas variables de entorno para confirmaciones por correo al agregar usuarios

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,7 @@
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
@@ -12,6 +13,14 @@ class Settings(BaseSettings):
     SECRET_KEY: SecretStr
     ALGORITHM: SecretStr
     ACCESS_TOKEN_EXPIRE_MINUTES: SecretStr
+    SECRET_KEY_CHECK_MAIL: SecretStr
+    SECURITY_PASSWD_SALT: SecretStr
+    DOMINIO: SecretStr
+    EMAIL_SERVER: SecretStr
+    EMAIL_PORT: SecretStr
+    EMAIL_USER: SecretStr
+    EMAIL_PASSWD: SecretStr
+
 
 # Carga de variables de entorno
-settings = Settings() 
+settings = Settings()
